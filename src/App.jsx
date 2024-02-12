e1d81432e1ebd5a23327475339f7f1e6e07febdc
Tai Phan <ptai7722@gmail.com>
2024-02-12 15:23:46 -0600

    Changed the main component from square to board, set up the initial board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 // export default identifies this function as the main component we are exporting from this module.
 
                       //A component is identified by having the first character of its identifier capitalized.
-export default function Square() {
+export default function Board() {
   //A component returns markup to its caller, it is what gives us the ability to nest components! As all components are simply returning markup that will be rendered by react-dom.
-  return <button className="square">X</button>;
+  return (
+    <div className="board">
+      <div className="board-row">
+        <button className="square">2</button>
+        <button className="square">3</button>
+        <button className="square">1</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </div>
+  );
 }
